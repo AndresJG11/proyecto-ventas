cb42d5e59977ebd0aba58275de9046f134155a3b
Andres JG <anjimenez11@hotmail.com>
2020-08-31 23:58:07 -0500

    electron reload

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ const { app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {
+
+    })
+}
+
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({})
+
+    mainWindow = new BrowserWindow({});
+
     mainWindow.loadURL(
         url.format({
             pathname: path.join(__dirname, 'views/index.html'),
             protocol: 'file',
             slashes: true
         })
-    )
-} );
+    );
+
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+
+});
