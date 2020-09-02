8d5fc0765e55f75add9386d65c6350a9754e9bad
Andres JG <anjimenez11@hotmail.com>
2020-09-02 00:39:21 -0500

    Header

--- renderer/components/Header.js
+++ renderer/components/Header.js
@@ -1,17 +1,55 @@
-import React           from 'react';
-import BaseComponent   from '../components/BaseComponent';
+import React from 'react';
+import BaseComponent from '../components/BaseComponent';
 
-class Header extends BaseComponent{
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 
-    constructor(props){
+
+class Header extends BaseComponent {
+
+    constructor(props) {
         super(props);
         this.typeField = "Header";
+        this.state = { searchInput: "" }
     }
+
+    handleOnClick = (e) => {
+        e.preventDefault();
+        const fromIdButton = e.target.id;
+        console.log(fromIdButton)
+    }
+
+    handleOnChangeInput = (e) => {
+        const readInput = e.target.value;
+        this.setState({ ...this.setState, searchInput: readInput })
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log("Buscar: " + this.state.searchInput);
+    }
+
+
+
     render() {
         return (
-			  <header>
-			  		SOY EL HEADER
-			  </header>
+            <header>
+                <h1 className="titulo texto-contraste"> PUNTO DE VENTA </h1>
+                <div className="header-top">
+                    <button type="button" className="btn"> Left </button>
+                    <form onSubmit={this.handleOnSubmit}>
+                        <input placeholder="Busqueda" type="text" className="input-text" onChange={this.handleOnChangeInput} value={this.state.searchInput} />
+                    </form>
+                    <button type="button" className="btn logout-btn">
+                        <MeetingRoomIcon className="btn-image" />
+                        <span className="btn-text"> Salir </span>
+                    </button>
+                </div>
+                <div className="header-bottom">
+                    <button type="button" className="btn" id="verProductos" onClick={this.handleOnClick}> Ver Productos </button>
+                    <button type="button" className="btn" id="vender" onClick={this.handleOnClick}> Vender </button>
+                    <button type="button" className="btn" id="generarReporte" onClick={this.handleOnClick}> Generar Reporte </button>
+                </div>
+            </header>
         );
     }
 }
