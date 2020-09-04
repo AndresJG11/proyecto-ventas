bf95f99f4f9fccf78503491ac73b55c6d4504daa
Andres JG <anjimenez11@hotmail.com>
2020-09-04 13:30:08 -0500

    modal agregar productos

--- renderer/pages/verProductos/index.js
+++ renderer/pages/verProductos/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Tabla from '../../components/Tabla';
 import BaseComponent from '../../components/BaseComponent'
+
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import RefreshIcon from '@material-ui/icons/Refresh';
-
 import ClearIcon from '@material-ui/icons/Clear';
+
+import Modal from '@material-ui/core/Modal';
+
 /*
 const simularProductos = (cantidad = 2) => {
     let productos = [];
@@ -22,32 +25,39 @@ const simularProductos = (cantidad = 2) => {
 
 const data = simularProductos(10);*/
 
-
 class GetProducts extends BaseComponent {
     constructor(props) {
         super(props);
 
-        this.state = { searchTable: ''}
-		  this.state.productos;
+        this.state = { searchTable: '', isModalOpen: false }
+        this.state.productos;
 
-        this.handleAddProduct = this.handleAddProduct.bind(this);
+        this.handleAddProduct = this.handleOpenModal.bind(this);
         this.handleOnChange = this.handleOnChange.bind(this);
         this.handleDeleteText = this.handleDeleteText.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
+        this.handleOpenModal = this.handleOpenModal.bind(this);
 
         this.textInput = React.createRef();
+
+        this.modalNombre = React.createRef();
+        this.modalCantidad = React.createRef();
+        this.modalCodigo = React.createRef();
     }
 
-	 async componentDidMount() {
-	  const response = await fetch('http://localhost:8888/api/products/get');
-	  const productsList = await response.json();
-	  this.setState({
-		  "productos" : productsList
-	  });
-	 }
+    async componentDidMount() {
+        const response = await fetch('http://localhost:8888/api/products/get');
+        const productsList = await response.json();
+        this.setState({
+            "productos": productsList
+        });
+    }
 
 
 
-    handleAddProduct(event) {
+    handleOpenModal(event) {
+        this.setState({ isModalOpen: true })
+
         /*const { productos } = this.state.data;
         const nuevoProducto = {
             id: productos[productos.length - 1]["id"] + 1,
@@ -58,6 +68,7 @@ class GetProducts extends BaseComponent {
         }
         productos.push(nuevoProducto)
         this.setState({ data: { productos } })*/
+
     }
 
     handleRefreshTable() {
@@ -74,14 +85,56 @@ class GetProducts extends BaseComponent {
         this.textInput.current.focus();
     }
 
+    handleModalClose() {
+        this.setState({ isModalOpen: false })
+    }
+
+
+    getAddProductForm() {
+        const handleAddProduct = (e) => {
+            e.preventDefault();
+            const nombre = this.modalNombre.current.value;
+            const cantidad = this.modalCantidad.current.value;
+            const codigo = this.modalCodigo.current.value;
+
+            console.log("Agregar", nombre, cantidad, codigo);
+
+            this.setState( {isModalOpen:false} )
+        }
+
+        return (<div className="modal-root">
+            <form onSubmit={handleAddProduct}>
+                <label>
+                    <span> Nombre: </span>
+                    <input type="text" className="modal-input" ref={this.modalNombre}/>
+                </label>
+                <label>
+                     <span> Cantidad: </span>
+                    <input type="number" className="modal-input number" ref={this.modalCantidad} />
+                </label>
+                <label>
+                    <span> Código: </span>
+                    <input type="number" className="modal-input number" ref={this.modalCodigo} />
+                </label>
+                <button className="btn btn-add ripple" type="submit">
+                    <AddBoxIcon className="btn-image" />
+                    <span className="btn-text"> Agregar </span>
+                </button>
+            </form>
+        </div>)
+    }
+
     render() {
 
-        const searchTable = this.state.searchTable;
-
+        const { searchTable, isModalOpen } = this.state;
 
         return (
             <div className="verProductos">
-                <button className="btn btn-add ripple" onClick={this.handleAddProduct}>
+                <Modal open={isModalOpen}
+                    onClose={this.handleModalClose}>
+                    {this.getAddProductForm()}
+                </Modal>
+                <button className="btn btn-add ripple" onClick={this.handleOpenModal}>
                     <AddBoxIcon className="btn-image" />
                     <span className="btn-text"> Añadir Producto </span>
                 </button>
@@ -95,15 +148,15 @@ class GetProducts extends BaseComponent {
                     <input type="text" className="input-table-search" ref={this.textInput} onChange={this.handleOnChange} value={this.state.searchTable} />
                 </div>
                 <hr className="style-two" />
-					 {
-						 (this.state.productos === undefined || this.state.productos === null)?
-						 	<center><h2>Buscando los productos</h2></center>
-						:
-							(this.state.productos.length === 0)?
-							<center><h2>No hay productos</h2></center>
-							:
-	                  <Tabla productos={this.state.productos}  />
-					 }
+                {
+                    (this.state.productos === undefined || this.state.productos === null) ?
+                        <center><h2>Buscando los productos</h2></center>
+                        :
+                        (this.state.productos.length === 0) ?
+                            <center><h2>No hay productos</h2></center>
+                            :
+                            <Tabla productos={this.state.productos} />
+                }
             </div>
         );
     }
