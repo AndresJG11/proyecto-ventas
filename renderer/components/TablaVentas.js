cb0f98a188391432fd746f7c2001ab9e18fff31d
Andres JG <anjimenez11@hotmail.com>
2020-09-05 15:18:03 -0500

    Ventas tablas

--- renderer/components/TablaVentas.js
+++ renderer/components/TablaVentas.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import DeleteIcon from '@material-ui/icons/Delete';
 
 class TablaVentas extends Component {
     constructor(props) {
         super(props)
+
+        this.handleDeleteProduct = this.handleDeleteProduct.bind(this);
     }
-    
+
     formatDate = (dateNro) => {
         let date = new Date(dateNro);
         return (`${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`)
@@ -12,30 +15,52 @@ class TablaVentas extends Component {
     handleSort = (e) => {
         const colToSort = e.target.id;
     }
+
+    handleDeleteProduct(productToDelete) {
+        let items = [...this.props.productos];
+        const indexToDelete = items.indexOf(productToDelete)
+        items.splice(indexToDelete, 1)
+        this.props.estado.setState({ productos: items });
+    }
+
     render() {
-        const {productos} = this.props;
+        const { productos } = this.props;
         let render;
-        if(productos.length > 0){
+        if (productos.length > 0) {
             render = <div>
-            <table className="verProductos-tabla">
+                <table className="verProductos-tabla">
+                    <thead>
+                        <tr>
+                            <th> Producto </th>
+                            <th> Cantidad </th>
+                            <th> Código de barras </th>
+                            <th> Eliminar </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {productos.map((producto) => <tr key={producto["id"]}>
+                            <td> {producto["nombre"]} </td>
+                            <td> {producto["cantidad"]} </td>
+                            <td> {producto["barras"]} </td>
+                            <td> <DeleteIcon onClick={(e) => this.handleDeleteProduct(producto)} style={{ cursor: 'pointer' }} id={producto['id']} /> </td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
+        } else {
+            render = <table className="verProductos-tabla">
                 <thead>
                     <tr>
                         <th> Producto </th>
                         <th> Cantidad </th>
                         <th> Código de barras </th>
+                        <th> Eliminar </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {productos.map((producto) => <tr key={producto["id"]}>
-                        <td> {producto["nombre"]} </td>
-                        <td> {producto["cantidad"]} </td>
-                        <td> {producto["barras"]} </td>
-                    </tr>)}
+                    <tr><td colspan="4" style={{textAlign:'center'}}> Sin Productos </td></tr>
                 </tbody>
             </table>
-        </div>
-        } else{
-            render = <p> Sin productos </p>
         }
         return (render)
     }
--- renderer/pages/vender/index.js
+++ renderer/pages/vender/index.js
@@ -14,7 +14,8 @@ class index extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { productos: [], productSelected: {}, dataSimulada: {} }
+        this.state = { productos: [], productSelected: {} }
+        //this.state = { productos: simularProductos(3).productos, productSelected: {} }
 
         this.handleProductChange = this.handleProductChange.bind(this)
         this.handleAgregarProducto = this.handleAgregarProducto.bind(this)
@@ -23,29 +24,42 @@ class index extends Component {
     }
 
     handleProductChange(e, productSelected) {
-        // Agregar un producto al hacer click en el boton
-        // Revisar si el producto seleccionado ya esta e incrementarlo
-
-        e.persist();
         this.setState({ productSelected });
-        //console.log('Estado. productoSeleccionado', this.state.productSelected.nombre)
-        //console.log('Argumento. productoSeleccionado', productoSeleccionado.nombre)
-
     }
 
     handleAgregarProducto(e) {
-        e.persist();
-        const { productSelected } = this.state
-        if (!!!productSelected) {
+        const { productos, productSelected } = this.state
+        const cantidad = this.cantidad.current.value;
+        let flagRepetido = false;
+
+
+        if (!!!productSelected || Object.keys(productSelected).length === 0 || cantidad === parseInt(cantidad, 10) || cantidad < 0) {
             return (null);
         }
 
-        this.setState( {...this.state, productos: this.state.productos.concat([productSelected])} )
+        productos.map((producto, idx) => {
+            if (producto.id === productSelected.id) {
+                flagRepetido = true;
+
+                let items = [...this.state.productos];
+                let item = { ...items[idx] };
+                item.cantidad = item.cantidad + parseInt(cantidad);
+                items[idx] = item;
+
+                this.setState({ productos: items });
+            }
+        })
+
+        if (!flagRepetido) {
+            let item = {...this.state.productSelected};
+            item.cantidad = parseInt(cantidad);
+            this.setState({ productos: productos.concat([item]) })
+        }
+
     }
 
-    componentDidMount(){
-        this.setState({dataSimulada: simularProductos(10)})
-    }
+    validar
+
 
     render() {
         const { productos } = this.state;
@@ -54,19 +68,17 @@ class index extends Component {
                 <div className="vender-buscador">
                     <Autocomplete
                         id="combo-box-demo"
-                        options={this.state.dataSimulada.productos}
+                        options={dataSimulada.productos}
                         onChange={this.handleProductChange}
                         className="vender-input"
                         clearText="Borrar"
-                        //clearOnEscape
-                        //clearOnBlur={false}
                         getOptionLabel={(option) => option.nombre}
                         renderInput={(params) => <TextField {...params} label="Buscar producto" variant="outlined" />}
                     />
                     <div className="vender-form">
                         <label className="label-number">
                             Cantidad
-                            <input type="number" className="input-number" ref={this.cantidad} />
+                            <input type="number" className="input-number" ref={this.cantidad} defaultValue={1} />
                         </label>
                         <button className="btn btn-add ripple" type="button" onClick={this.handleAgregarProducto}>
                             <AddBoxIcon className="btn-image" />
@@ -75,7 +87,7 @@ class index extends Component {
                     </div>
                 </div>
                 <div>
-                    <TablaVentas productos={productos} />
+                    <TablaVentas productos={productos} estado={this} />
                 </div>
             </div>
         )
@@ -96,107 +108,6 @@ const simularProductos = (cantidad = 2) => {
     return ({ productos })
 }
 
+const dataSimulada = simularProductos(10);
 
-const data = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
-    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-    { title: 'The Good, the Bad and the Ugly', year: 1966 },
-    { title: 'Fight Club', year: 1999 },
-    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
-    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
-    { title: 'Forrest Gump', year: 1994 },
-    { title: 'Inception', year: 2010 },
-    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
-    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { title: 'Goodfellas', year: 1990 },
-    { title: 'The Matrix', year: 1999 },
-    { title: 'Seven Samurai', year: 1954 },
-    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
-    { title: 'City of God', year: 2002 },
-    { title: 'Se7en', year: 1995 },
-    { title: 'The Silence of the Lambs', year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: 'Life Is Beautiful', year: 1997 },
-    { title: 'The Usual Suspects', year: 1995 },
-    { title: 'Léon: The Professional', year: 1994 },
-    { title: 'Spirited Away', year: 2001 },
-    { title: 'Saving Private Ryan', year: 1998 },
-    { title: 'Once Upon a Time in the West', year: 1968 },
-    { title: 'American History X', year: 1998 },
-    { title: 'Interstellar', year: 2014 },
-    { title: 'Casablanca', year: 1942 },
-    { title: 'City Lights', year: 1931 },
-    { title: 'Psycho', year: 1960 },
-    { title: 'The Green Mile', year: 1999 },
-    { title: 'The Intouchables', year: 2011 },
-    { title: 'Modern Times', year: 1936 },
-    { title: 'Raiders of the Lost Ark', year: 1981 },
-    { title: 'Rear Window', year: 1954 },
-    { title: 'The Pianist', year: 2002 },
-    { title: 'The Departed', year: 2006 },
-    { title: 'Terminator 2: Judgment Day', year: 1991 },
-    { title: 'Back to the Future', year: 1985 },
-    { title: 'Whiplash', year: 2014 },
-    { title: 'Gladiator', year: 2000 },
-    { title: 'Memento', year: 2000 },
-    { title: 'The Prestige', year: 2006 },
-    { title: 'The Lion King', year: 1994 },
-    { title: 'Apocalypse Now', year: 1979 },
-    { title: 'Alien', year: 1979 },
-    { title: 'Sunset Boulevard', year: 1950 },
-    { title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', year: 1964 },
-    { title: 'The Great Dictator', year: 1940 },
-    { title: 'Cinema Paradiso', year: 1988 },
-    { title: 'The Lives of Others', year: 2006 },
-    { title: 'Grave of the Fireflies', year: 1988 },
-    { title: 'Paths of Glory', year: 1957 },
-    { title: 'Django Unchained', year: 2012 },
-    { title: 'The Shining', year: 1980 },
-    { title: 'WALL·E', year: 2008 },
-    { title: 'American Beauty', year: 1999 },
-    { title: 'The Dark Knight Rises', year: 2012 },
-    { title: 'Princess Mononoke', year: 1997 },
-    { title: 'Aliens', year: 1986 },
-    { title: 'Oldboy', year: 2003 },
-    { title: 'Once Upon a Time in America', year: 1984 },
-    { title: 'Witness for the Prosecution', year: 1957 },
-    { title: 'Das Boot', year: 1981 },
-    { title: 'Citizen Kane', year: 1941 },
-    { title: 'North by Northwest', year: 1959 },
-    { title: 'Vertigo', year: 1958 },
-    { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
-    { title: 'Reservoir Dogs', year: 1992 },
-    { title: 'Braveheart', year: 1995 },
-    { title: 'M', year: 1931 },
-    { title: 'Requiem for a Dream', year: 2000 },
-    { title: 'Amélie', year: 2001 },
-    { title: 'A Clockwork Orange', year: 1971 },
-    { title: 'Like Stars on Earth', year: 2007 },
-    { title: 'Taxi Driver', year: 1976 },
-    { title: 'Lawrence of Arabia', year: 1962 },
-    { title: 'Double Indemnity', year: 1944 },
-    { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
-    { title: 'Amadeus', year: 1984 },
-    { title: 'To Kill a Mockingbird', year: 1962 },
-    { title: 'Toy Story 3', year: 2010 },
-    { title: 'Logan', year: 2017 },
-    { title: 'Full Metal Jacket', year: 1987 },
-    { title: 'Dangal', year: 2016 },
-    { title: 'The Sting', year: 1973 },
-    { title: '2001: A Space Odyssey', year: 1968 },
-    { title: "Singin' in the Rain", year: 1952 },
-    { title: 'Toy Story', year: 1995 },
-    { title: 'Bicycle Thieves', year: 1948 },
-    { title: 'The Kid', year: 1921 },
-    { title: 'Inglourious Basterds', year: 2009 },
-    { title: 'Snatch', year: 2000 },
-    { title: '3 Idiots', year: 2009 },
-    { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
 export default index;
